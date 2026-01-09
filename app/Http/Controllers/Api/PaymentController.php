5206b4ffc3699851f02b74d04f65c94502839989
Jemy15 <maulidinfirdaus05@gmail.com>
2026-01-09 17:26:51 +0800

    Memperbaiki konflik namespace di PaymentController dan update method store

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Payment;
 use App\Models\Booking;
